e5ac8c97b99d493bb9ec5780375f2e57ee63022c
Alao Roseline <rose@apples-MacBook-Pro-2.local>
2023-11-07 21:02:54 +0000

    I added main.py file and imported packages

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv('smoking.csv')
